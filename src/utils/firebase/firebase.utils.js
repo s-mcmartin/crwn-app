3595ddd29fd8252c687d5499c5f6a97dddda3682
s-mcmartin <shannoncmcguire@outlook.com>
2023-05-07 15:01:33 -0400

    Add categories to firebase, fetch to shop componen

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,16 @@ import {
   //observable listener
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDqyg4rXnMqvN1p-PWqzED932c7PmXOz9E",
@@ -37,6 +46,36 @@ export const signInWithGoogleRedirect = () =>
   signInWithGoogleRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "collections");
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+  console.log(categoryMap);
+  return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (userAuth, otherInfo = {}) => {
   if (!userAuth) return;
